658c7d874ea86682b353ef4d40ec54683e0766a1
Naresh Mepani <nareshmepani@gmail.com>
2025-07-18 14:25:45 +0000

    Fix: Remove allowedDevOrigins from next.config.ts

--- src/next.config.ts
+++ src/next.config.ts
@@ -32,11 +32,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    allowedDevOrigins: [
-        "https://6000-firebase-studio-1749052623784.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev",
-    ],
-  },
+  //experimental: {
+  //  allowedDevOrigins: [
+   //     "https://6000-firebase-studio-1749052623784.cluster-6vyo4gb53jczovun3dxslzjahs.cloudworkstations.dev",
+   // ],
+  //},
   webpack: (config, { isServer }) => {
 
     config.module.rules.push({
