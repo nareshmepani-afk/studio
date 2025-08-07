14b8bcee38d189ba95374cef78d90839d7175af9
Naresh Mepani <nareshmepani@gmail.com>
2025-08-07 18:13:00 +0000

    Request URL https://region1.google-analytics.com/g/collect?v=2&tid=G-13F

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,23 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
